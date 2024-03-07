3bc5a9fda923eb3981e417fbfe5a773942a20c31
Javi <54064297+Javiihg@users.noreply.github.com>
2024-03-07 11:16:27 +0000

    last act

--- Assets/Scripts/PlayerDestroy.cs
+++ Assets/Scripts/PlayerDestroy.cs
@@ -7,7 +7,7 @@ public class PlayerDestroy : MonoBehaviour
     public GameObject explosion;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Projectile"))
+        if (other.CompareTag("Projectile") || other.CompareTag("Terrain"))
         {
             Destroy(gameObject);
             Time.timeScale = 0f;
